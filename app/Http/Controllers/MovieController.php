c6a7ae76eeb8c86aafb99c752d6f260dd2cf7276
lukaszmosionek <mosioneklukasz@gmail.com>
2024-09-13 23:38:05 +0200

    MovieController

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,15 +2,43 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Log;
 
 class MovieController extends Controller
 {
-    public function getTitles(Request $request): JsonResponse
+    public function getTitles(Request $request)//: JsonResponse
     {
-        // TODO
 
-        return response()->json([]);
+        $timeOfCache = 60; //seconds
+
+        try{
+
+            $bar = array_column( (new \External\Bar\Movies\MovieService)->getTitles()['titles'], 'title');
+            $baz = (new \External\Baz\Movies\MovieService)->getTitles()['titles'];
+            $foo = (new \External\Foo\Movies\MovieService)->getTitles();
+
+        }catch(\Exception $e){
+
+            $e->getMessage();
+            Log::info( $e->getMessage() );
+
+            if( !Cache::get('titles') ){
+                return response()->json(['status'=> 'failure']);
+            }else{
+                return response()->json( Cache::get('titles') );
+            }
+
+        }
+
+        $results = array_merge($bar ,$baz ,$foo);
+
+        Cache::remember('titles', $timeOfCache, function () use($results) {
+            return $results;
+        });
+
+        return response()->json($results);
+
     }
 }
